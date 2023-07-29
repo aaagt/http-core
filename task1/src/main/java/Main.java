3113be38040f057bc52e400cc8091f89a1469c01
aaagt <example_e_mail@github_space_man.com>
2023-07-29 17:33:37 +0500

    Add closing http client

--- task1/src/main/java/Main.java
+++ task1/src/main/java/Main.java
@@ -8,38 +8,49 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        final var httpClient = getClient();
-        final var request = getRequest();
-        final var response = httpClient.execute(request);
-        final var json = response.getEntity().getContent();
+    public static void main(String[] args) {
+        useClient(httpClient -> {
+            try {
+                final var request = getRequest();
+                final var response = httpClient.execute(request);
+                final var json = response.getEntity().getContent();
 
-        final var objectMapper = new ObjectMapper();
-        final List<Cat> cats = objectMapper.readValue(
-                json,
-                new TypeReference<>() {
-                }
-        );
+                final var objectMapper = new ObjectMapper();
+                final List<Cat> cats = objectMapper.readValue(
+                        json,
+                        new TypeReference<>() {}
+                );
 
-        cats.stream()
-                .filter(cat -> cat.getUpvotes() != null && cat.getUpvotes() > 0)
-                .forEach(System.out::println);
+                cats.stream()
+                        .filter(cat -> cat.getUpvotes() != null && cat.getUpvotes() > 0)
+                        .forEach(System.out::println);
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+        });
+
     }
 
     public static HttpUriRequest getRequest() {
         return new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
     }
 
-    public static CloseableHttpClient getClient() {
-        return HttpClientBuilder.create()
+    public static void useClient(Consumer<CloseableHttpClient> block) {
+        try (var httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
                         .setSocketTimeout(30000)    // максимальное время ожидания получения данных
                         .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
                         .build())
-                .build();
+                .build()
+        ) {
+            block.accept(httpClient);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
